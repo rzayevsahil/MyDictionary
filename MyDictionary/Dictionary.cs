f22ac026881b79dcd689aa6dec196377b545aa96
Sahil Rzayev <sahilrzayev2000@gmail.com>
2021-01-23 02:05:25 +0400

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<T1, T2>
+    {
+        T1[] keys;
+        T2[] values;
+
+        public Dictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempKeys = keys;
+            T2[] tempValues = values;
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public T1[] Keys
+        {
+            get { return keys; }
+        }
+
+        public T2[] Values
+        {
+            get { return values; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> lessons = new Dictionary<int, string>();
+            lessons.Add(1, "C#");
+            lessons.Add(2, "Java");
+            lessons.Add(3, "Html");
+            lessons.Add(4, "C++");
+            lessons.Add(5, "Phyton");
+
+            Console.WriteLine("Disctionary size : "+lessons.Count);
+            for (int i = 0; i < lessons.Count; i++)
+            {
+                Console.WriteLine(lessons.Keys[i] + " --> " + lessons.Values[i]);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
